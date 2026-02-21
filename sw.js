e4c8d8a386bf2d1a593c30acf85eecd8d1d89b1b
amal198704-hazel <amal198704@gmail.com>
2026-02-21 06:21:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener("install", e => {
-    console.log("Service Worker Ready");
+/* ⭐ SAFE SERVICE WORKER FOR MAP + PWA */
+
+self.addEventListener("install", () => {
+    self.skipWaiting();
 });
+
+self.addEventListener("activate", () => {
+    self.clients.claim();
+});
+
+/* DO NOT INTERCEPT NETWORK REQUESTS */
